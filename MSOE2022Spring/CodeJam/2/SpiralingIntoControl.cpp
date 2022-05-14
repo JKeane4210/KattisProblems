acf0d6c4a25a26c6591895f131294cdc6796f8ce
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-05-14 16:10:31 -0500

    First Code Jam round 2 in the books (rank 2803)

---
+++ MSOE2022Spring/CodeJam/2/SpiralingIntoControl.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Jonny Keane on 5/14/22.
+//
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+
+#define V vector
+#define vi V<int>
+#define vll V<ll>
+#define vd V<double>
+#define pii pair<int, int>
+#define pll pair<ll,ll>
+#define vpii V<pii>
+#define vpll V<pll>
+#define graph V<vi>
+#define wgraph V<V<pii>>
+
+// loops
+#define WHILE(n) while(n--)
+#define FOR(a) for(ll i=0;i<a;i++)
+#define FIND(a, e) find(a.begin(), a.end(), e)
+#define minimum(a) *min_element(a.begin(), a.end())
+#define maximum(a) *max_element(a.begin(), a.end())
+
+#define nl '\n'
+
+// use M_PI for PI (imported from cmath within stdc++.h)
+
+using namespace std;
+
+// Lessons from Today's round:
+// - Learn how to step back when things are going wrong
+//  - Hopefully this is not related with my set up, but unpredictable things happen and I have to react
+// - If you can solve problems for points, do it. Would have qualified if I had just brute forced easy cases
+//      on both problems and then seen what I could have scraped together to get myself to qualification
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // code here
+    cout << "Hello World" << endl;
+
+    return 0;
+}
